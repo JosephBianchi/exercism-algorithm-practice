b8b64edcac875709f009aebfde9614d1736e1431
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-11-04 16:05:39 -0400

    convert dna to rna strand - transcription in python

---
+++ python/rna-transcription/rna_transcription.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+def to_rna(dna_strand):
+    dna_to_rna = {
+        "G":"C", "C":"G", "T":"A", "A":"U"
+    }
+    rna = ''
+    for nucleotide in dna_strand:
+        rna += dna_to_rna.get(nucleotide)
+    return rna
+
+
+
+
+print(to_rna('GCTA'))
